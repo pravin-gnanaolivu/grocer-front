427e8084f0254bbb4a731d1f27f874bce11ccf88
pravin-gnanaolivu <57227620+pravin-gnanaolivu@users.noreply.github.com>
2020-12-17 13:15:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,9 @@ import ReactDOM from "react-dom"
 
 import App from "./components/App"
 import "semantic-ui-css/semantic.min.css"
-import { Offline, Online } from "react-detect-offline"
-import InternetConnectionError from "./components/InterConError"
 
 require("dotenv").config()
 
-ReactDOM.render(
-  <>
-    <Online>
-      <App />
-    </Online>
-    <Offline>
-      <InternetConnectionError />
-    </Offline>
-  </>,
+ReactDOM.render(<App />,
   document.querySelector("#root")
 )
